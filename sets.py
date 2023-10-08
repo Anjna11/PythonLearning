8b63670bf04f7d0e8210403dd52a2d2b48bdfe80
Anjna11 <anjnajagatiya@gmail.com>
2023-10-08 21:21:53 +0530

    adding set demo

---
+++ sets.py
@@ -0,0 +1,14 @@
+s1 = {"Apple", "Banana", "Cherry"}
+s2 = {"google", "Microsoft", "Apple"}
+
+print("Set1: ", s1)
+print("Set2: ", s2)
+
+#add opration in s1
+s1.add("Mengo")
+print("After Add in Set1: ", s1)
+
+#update s1
+s2.update(s1)
+print("After Update in Set2: ", s2)
+
